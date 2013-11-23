76a3c4325271ff797ef4726165d157197e8d76a2
kkkon <kiyofumi.kondoh+github@gmail.com>
2013-11-23 21:53:12 +0900

    initial

---
+++ src/main/java/jp/ne/sakura/kkkon/StripElfSectionHeader/App.java
@@ -0,0 +1,13 @@
+package jp.ne.sakura.kkkon.StripElfSectionHeader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
